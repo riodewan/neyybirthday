0b212887d1978f504d9f5e7a76a9360352a94a52
riodewannn <muhammadriodewantoro@gmail.com>
2024-07-18 15:50:39 +0700

    first commit

---
+++ js/index.js
@@ -0,0 +1,14 @@
+$('#login-button').click(function (event) {
+	var userName=document.getElementById("userName").value;
+    var pwd=document.getElementById("pwd").value;
+
+    if(userName=="neyyy" &&  pwd=="sayangio"){
+			event.preventDefault();
+			$('form').fadeOut(500);
+			$('.wrapper').addClass('form-success');
+			setTimeout(function(){location.href="BirthdayCake.html";},2000);
+		}
+	else{
+		alert("Password Salah");
+	}
+});
